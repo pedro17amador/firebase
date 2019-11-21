cc022b8bae7e579535c972e490acbcf8a9c14d68
pedro17amador <pedro.r.amador98@gmail.com>
2019-11-21 19:53:47 -0300

    FireBase

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+// Initialize Firebase
+var fbConfig = {
+   apiKey: "AIzaSyA9tGQW6OKvLtkQWcLnMGh4LM8mXhKbtDQ",
+    authDomain: "narnias-49b51.firebaseapp.com",
+    databaseURL: "https://narnias-49b51.firebaseio.com",
+    projectId: "narnias-49b51",
+    storageBucket: "narnias-49b51.appspot.com",
+    messagingSenderId: "283289246282",
+    appId: "1:283289246282:web:b3edc5020f74cd4e36a4b3"
+};
+
